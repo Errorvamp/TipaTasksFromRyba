cd51534b471577609b98a599e724d81ca5bba434
Errorvamp <144660996+Errorvamp@users.noreply.github.com>
2025-05-12 20:43:39 +0300

    Add files via upload

---
+++ WindowsForms/1 Лабораторная/Ex 1-3/Лаба1/FormTreyg.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Лаба1
+{
+    partial class FormTreyg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(60, 53);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(135, 40);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "Close Form";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormTreyg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormTreyg";
+            this.Text = "FormTreyg";
+            this.Load += new System.EventHandler(this.FormTreyg_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.Button buttonClose;
+    }
+}
---
+++ WindowsForms/1 Лабораторная/Ex 1-3/Лаба1/nForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Лаба1
+{
+    public partial class nForm : Лаба1.FormTreyg
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Main()
+        {
+            Application.Run(new nForm());
+        }
+    }
+}
