3c17817d081ba55e14e1c6835b34921e00c4d335
Errorvamp <144660996+Errorvamp@users.noreply.github.com>
2025-05-13 16:28:47 +0300

    Add files via upload

---
+++ WindowsForms/1 Лабораторная/Ex  4/Лаба1Упр4/ParentForm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаба1Упр4
+{
+    public partial class ParentForm : Form
+    {
+        private int openDocuments = 0;
+        public ParentForm()
+        {
+            InitializeComponent();
+            spData.Text =
+        Convert.ToString(System.DateTime.Today.ToLongDateString());
+
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+            spWin.Text = "Windows is cascade";
+            spWin.Text = "Windows is horizontal";
+
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+
+        private void WindowTitleMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.MdiParent = this;
+            newChild.Text = newChild.Text+" "+ ++openDocuments;
+            newChild.Show();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, EventArgs e)
+        {
+        }
+        private void NewDoc_Click(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.MdiParent = this;
+            newChild.Show();
+            newChild.Text = newChild.Text + " " +
+            ++openDocuments;
+        }
+        private void Cascade_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+        private void Title_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi
+                    (System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
