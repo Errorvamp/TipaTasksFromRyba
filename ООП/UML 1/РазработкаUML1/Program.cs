ed2cb49fa04892df812a46add2ca31eff6129840
Errorvamp <144660996+Errorvamp@users.noreply.github.com>
2025-05-14 14:41:08 +0300

    Add files via upload

---
+++ ООП/UML 1/РазработкаUML1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace РазработкаUML1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Население страны равно M млн чел., количество врачей – B млн чел.
+            //Прогнозируется ежегодный рост населения на р % и ежегодное увеличение 
+            //числа врачей на Q тыс. специалистов. Найти количество врачей на 
+            //100 тыс. чел населения для каждого из ближайших десяти лет
+            int M = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int Q = int.Parse(Console.ReadLine());
+            int futurepp = M * p / 100;
+            int doconp = (futurepp / (B * Q)) / 100000;
+            Console.WriteLine(doconp);
+        }
+    }
+}
---
+++ ООП/UML 1/РазработкаUML1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("РазработкаUML1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("РазработкаUML1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("caad76e6-a996-4011-9d10-3b1f2139c939")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
