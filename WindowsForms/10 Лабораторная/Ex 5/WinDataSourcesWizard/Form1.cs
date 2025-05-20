a16949328025d8853ec52329a7936c944af6e4f0
Errorvamp <144660996+Errorvamp@users.noreply.github.com>
2025-05-20 14:04:04 +0300

    Add files via upload

---
+++ WindowsForms/10 Лабораторная/Ex 5/WinDataSourcesWizard/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinDataSourcesWizard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void personBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.personBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.forlabaDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "forlabaDataSet.Question". При необходимости она может быть перемещена или удалена.
+            this.questionTableAdapter.Fill(this.forlabaDataSet.Question);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "forlabaDataSet.Question". При необходимости она может быть перемещена или удалена.
+            this.questionTableAdapter.Fill(this.forlabaDataSet.Question);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "forlabaDataSet.Person". При необходимости она может быть перемещена или удалена.
+            this.personTableAdapter.Fill(this.forlabaDataSet.Person);
+
+        }
+    }
+}
