69b4cd60c65d724ffd8f4e31b3ad4072038e5151
Errorvamp <144660996+Errorvamp@users.noreply.github.com>
2025-05-21 22:38:36 +0300

    Add files via upload

---
+++ ASPNET/Лабораторная 1/Controllers/Laba1Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAspnet1.Controllers
+{
+    public class Laba1Controller : Controller
+    {
+        // GET: Laba1
+        public ActionResult FirstViewMethod()
+        {
+            ViewBag.Vegetables = GetVegetablesList();
+            return View();
+        }
+        public ActionResult SecondViewMethod()
+        {
+            return View(GetVegetablesList().OrderBy(x => x).ToList());
+        }
+        public ActionResult ThirdViewMethod()
+        {
+            return View(GetVegetablesList().OrderBy(x => x).ToList());
+        }
+        public List<string> GetVegetablesList()
+        {
+            List<string> vegetables = new List<string>();
+            vegetables.Add("Томат");
+            vegetables.Add("Огурец");
+            vegetables.Add("Картофель");
+            vegetables.Add("Кабачок");
+            vegetables.Add("Баклажан");
+            vegetables.Add("Капуста");
+            vegetables.Add("Брокколи");
+
+            return vegetables;
+        }
+    }
+}
